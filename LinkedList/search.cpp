f415c863462645055e2cd6ae7e2fa277a19418a0
corebyte56 <fahim789m@gmail.com>
2026-03-26 23:24:03 +0600

    feat: add linear search implementation

---
+++ LinkedList/search.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int key, found = 0;
+
+    cout << "Enter number to search: ";
+    cin >> key;
+
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] == key) {
+            cout << "Found at index: " << i << endl;
+            found = 1;
+            break;
+        }
+    }
+
+    if (found == 0) {
+        cout << "Not Found" << endl;
+    }
+
+    return 0;
+}
